6f183ae46e2755beb97181f12c200b789ca863b1
Brad Kajganich <BKajganich6321@conestogac.on.ca>
2025-11-29 23:38:17 -0500

    here

---
+++ Logger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwnWebServer
+{
+    public class LogWriter
+    {
+        private string logPath = string.Empty;
+
+        public LogWriter(string logMessage)
+        {
+            LogWrite(logMessage);
+        }
+
+
+        public void LogWrite(string logMessage)
+        {
+            logPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            try
+            {
+                using (StreamWriter w = File.AppendText(logPath + "\\" + "log.txt"))
+                {
+                    Log(logMessage, w);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Logging File Exception: " + ex.ToString());
+            }
+        }
+
+        public void Log(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.Write("\r\nLog Entry : ");
+                txtWriter.WriteLine("{0}", DateTime.Now);
+                txtWriter.WriteLine("  :");
+                txtWriter.WriteLine("  :{0}", logMessage);
+                txtWriter.WriteLine("-------------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Logging Writing Exception: " + ex.ToString());
+            }
+        }
+    }
+}
